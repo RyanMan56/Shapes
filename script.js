b2a56c0f7b9f12a37e82f0d9d569202cb84436ae
Ryan Jones <RyanMan56@gmail.com>
2017-09-14 12:54:03 +0100

    Global scale

--- script.js
+++ script.js
@@ -39,23 +39,6 @@ function filledCircle() {
   ctx.closePath();
 }
 
-function pythagoreanCircle() {
-  var origin = new Vector2(canvas.width / 2, canvas.height / 2);
-  var splits = currentSplits;
-  var scaler = 400;
-  var degreesPerSplit = 360 / splits;
-  for (var d = 0; d < splits; d++) {
-    var deg = Math.ceil(d * degreesPerSplit);
-    var deg2 = Math.ceil((d + 1) * degreesPerSplit);
-
-    drawTriangle(
-      origin,
-      new Vector2(origin.x + Math.sin(deg * toRads) * scaler, origin.y + Math.cos(deg * toRads) * scaler),
-      new Vector2(origin.x + Math.sin(deg2 * toRads) * scaler, origin.y + Math.cos(deg2 * toRads) * scaler)
-    );
-  }
-}
-
 function drawBackground() {
   ctx.fillStyle = "#413C58";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
